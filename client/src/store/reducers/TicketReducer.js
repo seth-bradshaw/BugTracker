282ce46acbda0ad118aaf245874495868ecefdaa
Seth Bradshaw <seth.bradshaw27@gmail.com>
2021-02-12 18:31:43 -0700

    syncing with main

--- client/src/store/reducers/TicketReducer.js
+++ client/src/store/reducers/TicketReducer.js
@@ -2,10 +2,17 @@ import {
   GETUSERTICKETSSTART,
   GETUSERTICKETSSUCCESS,
   GETUSERTICKETSFAILURE,
+  FILTERTICKETSNOTSTARTED, 
+  FILTERTICKETSINPROGRESS,
+  FILTERTICKETSCOMPLETED
 } from '../../constants';
 
 const defaultState = {
   tickets: [],
+  notStarted: false,
+  inProgress: false,
+  completed: false,
+  displayAllTickets: true
 };
 
 const ticketReducer = (state = defaultState, action) => {
@@ -16,6 +23,24 @@ const ticketReducer = (state = defaultState, action) => {
         tickets: action.payload,
         isFetching: false,
       };
+    case FILTERTICKETSNOTSTARTED:
+      return {
+        ...state,
+        notStarted: action.payload, 
+        displayAllTickets: false
+      };
+    case FILTERTICKETSINPROGRESS:
+      return {
+        ...state,
+        inProgress: action.payload,
+        displayAllTickets: false
+      };
+    case FILTERTICKETSCOMPLETED:
+      return {
+        ...state,
+        completed: action.payload,
+        displayAllTickets: false
+      }
     default:
       return state;
   }
