0b1ebb7a705eebabbb14c58bf41390a45a3bf3df
Seth Bradshaw <seth.bradshaw27@gmail.com>
2021-02-24 23:13:49 -0700

    got company users custom query to work

--- server/src/main/java/com/portfolio/bugtracker/services/UserServiceImpl.java
+++ server/src/main/java/com/portfolio/bugtracker/services/UserServiceImpl.java
@@ -8,6 +8,8 @@ import com.portfolio.bugtracker.models.UserRoles;
 import com.portfolio.bugtracker.repositories.RoleRepository;
 import com.portfolio.bugtracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
